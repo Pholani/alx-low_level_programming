e75c8d5c57bdd4702207749da4c134f95fb94588
Pholani <pholanimavuso@gmail.com>
2023-02-23 15:06:01 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -6,7 +6,7 @@
  *@b: The second number to multiply with the first number
  *Return: The result between two integers x and y
  */
-int nul(int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
